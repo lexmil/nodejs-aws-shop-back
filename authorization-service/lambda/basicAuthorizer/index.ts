5944f39c9307b45f2ac7ce547012d42ff28620fc
lexmil <lexmil@github.com>
2025-03-26 14:58:44 +0300

    Fixed: authorization token parser

--- authorization-service/lambda/basicAuthorizer/index.ts
+++ authorization-service/lambda/basicAuthorizer/index.ts
@@ -1,4 +1,5 @@
-// In your authorization service lambda (basicAuthorizer)
+import { APIGatewayAuthorizerResult } from "aws-lambda";
+
 export const handler = async (event: any) => {
   console.log("Event: ", event);
 
@@ -6,47 +7,56 @@ export const handler = async (event: any) => {
     const { authorizationToken, methodArn } = event;
 
     // If no token provided or wrong format
-    if (
-      !authorizationToken ||
-      !authorizationToken.toLowerCase().startsWith("basic ")
-    ) {
-      throw new Error("Unauthorized"); // This will trigger 401
+    if (!authorizationToken) {
+      return generatePolicy("user", "Deny", event.methodArn, {
+        statusCode: 401,
+      });
     }
 
     const encodedCreds = authorizationToken.split(" ")[1];
     const buff = Buffer.from(encodedCreds, "base64");
-    const plainCreds = buff.toString("utf-8").split(":");
+    const plainCreds = buff.toString("utf-8").split("=");
     const username = plainCreds[0];
     const password = plainCreds[1];
 
-    console.log(`username: ${username}`);
-
-    const storedUserName = process.env.LOGIN;
+    const storedUsername = process.env.LOGIN;
     const storedPassword = process.env.PASSWORD;
 
     // Check credentials
-    if (username !== storedUserName || password !== storedPassword) {
-      throw new Error("Unauthorized"); // This will trigger 401
+    if (username !== storedUsername || password !== storedPassword) {
+      return generatePolicy("user", "Deny", event.methodArn, {
+        statusCode: 403,
+      });
     }
 
-    // If credentials are valid, return Allow policy
-    return {
-      principalId: username,
-      policyDocument: {
-        Version: "2012-10-17",
-        Statement: [
-          {
-            Action: "execute-api:Invoke",
-            Effect: "Allow",
-            Resource: methodArn,
-          },
-        ],
-      },
-    };
+    return generatePolicy("user", "Allow", event.methodArn, {
+      statusCode: 200,
+    });
   } catch (error) {
-    console.error("Authorization error:", error);
-
-    // Instead of returning a policy, throw an error to trigger 401
-    throw new Error("Unauthorized");
+    return generatePolicy("user", "Deny", event.methodArn, {
+      statusCode: 500,
+    });
   }
+};
+
+const generatePolicy = (
+  principalId: string,
+  effect: "Allow" | "Deny",
+  resource: string,
+  context: any = null,
+): APIGatewayAuthorizerResult => {
+  return {
+    principalId: principalId,
+    policyDocument: {
+      Version: "2012-10-17",
+      Statement: [
+        {
+          Action: "execute-api:Invoke",
+          Effect: effect,
+          Resource: resource,
+        },
+      ],
+    },
+    context,
+  };
 };
